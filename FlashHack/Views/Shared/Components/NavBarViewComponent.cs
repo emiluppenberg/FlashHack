704c1e50524a56351521fc0f1b5119bc9fa97e3c
Cal <151396661+CalvinJagare@users.noreply.github.com>
2025-02-21 09:10:26 +0100

    navbar done

---
+++ FlashHack/Views/Shared/Components/NavBarViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using FlashHack.Data;
+using FlashHack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FlashHack.Views.Shared.Components
+{
+    public class NavBarViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NavBarViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var headCategories = await _context.HeadCategory
+                .Include(hc => hc.SubCategories)
+                .ToListAsync();
+
+            var viewModel = new NavBarViewModel
+            {
+                HeadCategories = headCategories
+            };
+
+            return View(viewModel);
+        }
+    }
+}
