2d8c2cb845c555eb11d137cb8445a3b8015fb890
Arlind Kaciku <arlindkaciku18@gmail.com>
2025-02-21 10:18:48 +0100

    Changed UserController

--- FlashHack/Controllers/UsersController.cs
+++ FlashHack/Controllers/UsersController.cs
@@ -7,58 +7,50 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FlashHack.Data;
 using FlashHack.Models;
+using FlashHack.Data.DataInterfaces;
 
 namespace FlashHack.Controllers
 {
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IUserRepository _userRepository;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(ApplicationDbContext context, IUserRepository userRepository)
         {
             _context = context;
+            _userRepository = userRepository;
         }
 
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            return View(await _context.User.ToListAsync());
+            var users = await _userRepository.GetAllAsync();
+            return View(users);
         }
 
         // GET: Users/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+            var user = await _userRepository.GetByIdAsync(id.Value);
+            if (user == null) return NotFound();
 
             return View(user);
         }
 
         // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        public IActionResult Create() => View();
 
         // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Employer,PhoneNumber,Email,Password,Bio,ProfilePicURL,Rating,Signature,IsPremium,IsAdmin,ShowEmail,ShowPhoneNumber,ShowEmployer,ShowToRecruiter,ShowRating")] User user)
+        public async Task<IActionResult> Create([Bind("FirstName,LastName,Employer,PhoneNumber,Email,Password,Bio,ProfilePicURL,Rating,Signature,IsPremium,IsAdmin,ShowEmail,ShowPhoneNumber,ShowEmployer,ShowToRecruiter,ShowRating")] User user)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(user);
+                _context.User.Add(user); // Använder _context för att lägga till direkt
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
@@ -68,48 +60,31 @@ namespace FlashHack.Controllers
         // GET: Users/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+            var user = await _userRepository.GetByIdAsync(id.Value);
+            if (user == null) return NotFound();
+
             return View(user);
         }
 
         // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Employer,PhoneNumber,Email,Password,Bio,ProfilePicURL,Rating,Signature,IsPremium,IsAdmin,ShowEmail,ShowPhoneNumber,ShowEmployer,ShowToRecruiter,ShowRating")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Employer,PhoneNumber,Email,Bio,ProfilePicURL,Rating,Signature,IsPremium,IsAdmin,ShowEmail,ShowPhoneNumber,ShowEmployer,ShowToRecruiter,ShowRating")] User user)
         {
-            if (id != user.Id)
-            {
-                return NotFound();
-            }
+            if (id != user.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
+                    await _userRepository.Update(user); // Uppdaterar via repository
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!await UserExists(user.Id)) return NotFound();
+                    throw;
                 }
                 return RedirectToAction(nameof(Index));
             }
@@ -119,17 +94,10 @@ namespace FlashHack.Controllers
         // GET: Users/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+            var user = await _userRepository.GetByIdAsync(id.Value);
+            if (user == null) return NotFound();
 
             return View(user);
         }
@@ -139,19 +107,19 @@ namespace FlashHack.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var user = await _context.User.FindAsync(id);
+            var user = await _userRepository.GetByIdAsync(id);
             if (user != null)
             {
-                _context.User.Remove(user);
+                _context.User.Remove(user); // Tar bort via _context
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool UserExists(int id)
+        private async Task<bool> UserExists(int id)
         {
-            return _context.User.Any(e => e.Id == id);
+            return await _userRepository.GetByIdAsync(id) != null;
         }
     }
 }
