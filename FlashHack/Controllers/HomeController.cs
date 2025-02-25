afa81b22ac0a2a52ceb65b823eb608fbcf35613e
twofourZ <uppenberg95@gmail.com>
2025-02-25 09:51:48 +0100

    test2

--- FlashHack/Controllers/HomeController.cs
+++ FlashHack/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace FlashHack.Controllers
                 HeadCategories = headCategories
             };
 
-            return View();
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
